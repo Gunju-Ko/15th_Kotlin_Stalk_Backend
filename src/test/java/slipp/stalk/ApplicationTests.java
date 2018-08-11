c9a1105fda13564bf3537b7c0b8e7eb6b91f47a0
gunjuKo <gunju@sk.com>
2018-08-11 11:27:43 +0900

    Initial Commit
    * TODO
    * - checkstyle, jacoco plugin 적용
    * - Datasource, JPA Setting
    * - Sample API 개발

---
+++ src/test/java/slipp/stalk/ApplicationTests.java
@@ -0,0 +1,16 @@
+package slipp.stalk;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
