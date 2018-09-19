a98ae999f346d17d2fbb275b37c0082646332d8a
gunjuKo <gunju@sk.com>
2018-09-20 00:08:40 +0900

    serviceAccountKey.json 파일이 존재하지 않는 경우 FakeMessageSender 사용

--- src/main/java/slipp/stalk/infra/firebase/FirebaseConfiguration.java
+++ src/main/java/slipp/stalk/infra/firebase/FirebaseConfiguration.java
@@ -1,14 +1,20 @@
 package slipp.stalk.infra.firebase;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.messaging.Message;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import slipp.stalk.service.messaging.MessageSender;
+import slipp.stalk.service.messaging.Response;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
+@Slf4j
 @Configuration
 public class FirebaseConfiguration {
 
@@ -16,10 +22,13 @@ public class FirebaseConfiguration {
 
     @Bean
     public MessageSender messageSender() {
+        if (FirebaseApp.getApps().isEmpty()) {
+            log.warn("Using FakeMessageSender, You cannot send firebase message");
+            return new FakeMessageSender();
+        }
         return new FirebaseMessageSender();
     }
 
-    @Slf4j
     public static class FirebaseInitializerInvoker implements ApplicationListener<ApplicationEnvironmentPreparedEvent> {
         @Override
         public void onApplicationEvent(ApplicationEnvironmentPreparedEvent event) {
@@ -30,9 +39,24 @@ public class FirebaseConfiguration {
             } catch (IllegalStateException e) {
                 log.warn("Firebase app has already been initialized.");
             } catch (IOException e) {
-                log.error("Exception is occur while initialize firebase app");
-                throw new IllegalStateException(e);
+                log.warn("Exception is occur while initialize firebase app : {}", e.getMessage());
             }
         }
     }
+
+    public class FakeMessageSender implements MessageSender {
+
+        @Override
+        public Response send(Message message) {
+            return new Response(Response.Result.FAIL, "Fail to send message");
+        }
+
+        @Override
+        public List<Response> send(List<Message> messages) {
+            return messages.stream()
+                           .map(this::send)
+                           .collect(Collectors.toList());
+        }
+    }
+
 }
